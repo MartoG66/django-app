dbe63597c067b4156f053e19cc0607dcd6c260c6
MartoG66 <116147737+MartoG66@users.noreply.github.com>
2023-03-13 10:57:32 +0200

    added posts to DB

--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView # new
+from .views import HomePageView, AboutPageView, PostPageView # new
 
 urlpatterns = [
      path("about/", AboutPageView.as_view(), name="about"), # new
      path("", HomePageView.as_view(), name="home"),
+     path("post/", PostPageView.as_view(), name="post"),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
+
+
+class PostPageView(ListView):
+    model = Post
+    template_name = "post.html"
 
 
 class HomePageView(TemplateView):
